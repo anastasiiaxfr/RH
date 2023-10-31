81e3ae1dc6f199012281aba832ce3b49926412c7
anastasiiaxfr <anastasiiaberest@gmail.com>
2023-10-31 22:32:48 +0200

    fix

---
+++ fe/src/pages/500.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Text } from '@chakra-ui/react'
+
+function Page() {
+    return (
+        <div className="container">
+            <Text
+                as="h1"
+                fontSize={{ base: '1.5rem', md: '1.5rem', lg: '1.5rem' }}
+                lineHeight={1}
+                mb={4}
+                fontWeight="bold"
+                letterSpacing="wider"
+            >
+                500 ERROR
+            </Text>
+        </div>
+    );
+}
+
+export default Page;
